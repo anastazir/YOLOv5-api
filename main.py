13af437796bd3181f5dae80a7ec2adc8e9501265
Anas Tazir <atazir2018@gmail.com>
2022-05-04 00:04:43 +0530

    add route for file detection

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import cv2
 import json
 import requests
-import numpy    as np
 from skimage    import io
 from flask      import Flask, request
 from flask_cors import CORS
@@ -24,12 +23,44 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-def transform(URL, img_size = 320, int8 = False):
-    int_type = np.int8 if int8 else np.float32
-    img = io.imread(URL)
-    im = cv2.resize(img, (img_size, img_size), 3).astype(int_type)
-    im = np.expand_dims(im, axis=0)/255.0
-    return img, im
+
+@app.route('/fileRoute', methods=['POST'])
+def fileRouter():
+    if request.method != 'POST':
+        return {"The request must be a POST request"}
+
+    try:
+        data = json.loads(request.data) # for React
+
+    except:
+        data = request.form  # for postman
+
+    if not data['base64']:
+
+        return  {'data': 'unable to  read file'}
+
+    int8 = True if data['int8'] == 'True' else False
+    type = 1 if data['type'] == '1' else 2
+
+    classes = CLASSES1 if type == 1 else CLASSES2
+
+    img, im = base_to_array(data, int8=int8)
+
+    MODEL_PATH = f'tflite_models/custom_int80{type}.tflite' if int8 else f'tflite_models/custom0{type}.tflite'
+    YOLO = Yolo(model_path = MODEL_PATH, CLASSES = classes, int8 = int8)
+
+    H = img.shape[0]
+    W = img.shape[1]
+
+    YOLO.pred(im)
+    YOLO.extract_results()
+    YOLO.return_bbox(iou_threshold = 0.0)
+    data = YOLO.return_results(H, W)
+
+    del YOLO
+
+    return data
+
 
 @app.route('/urlRoute', methods=['POST'])
 def urlRoute():
