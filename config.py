582f69695cb531577f988a4ba2c9c16fe825332f
Anas Tazir <atazir2018@gmail.com>
2022-05-12 02:11:18 +0530

    add new file to save constants

---
+++ config.py
@@ -0,0 +1,4 @@
+CLASSES = [[ "Bird", "Cat", "Dog", "Flower", "Face" ],
+["Insect", "Fish", "Fast_food", "Animal", "Fruit", "Traffic_light", "Vehicle_registration_plate", "Car", "Weapon"],
+["Animal" ,"Bird" ,"Cat" ,"Dog" ,"Fish" ,"Flower" ,"Human face" ,"Insect"],
+['Airplane', 'Ambulance', 'Bicycle', 'Boat', 'Bus', 'Car', 'Helicopter', 'Jet ski', 'Limousine', 'Motorcycle', 'Taxi', 'Train']]
